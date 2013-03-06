ee13d23676908e756f80e89eb998578b128adfcc
jaxbot <jaxbot@gmail.com>
2013-03-06 14:03:17 -0500

    Initial release

---
+++ README.md
@@ -0,0 +1,14 @@
+WPH EmailKey
+==========
+
+Windows Phone homebrew app like DoubleAt, by Jaxbot
+
+Download releases @ http://windowsphonehacker.com
+
+Licensed under GPLv3. In (unofficial) laymen's terms, you can use this source for any purpose, so long as your software
+remains under the same license. You can use the use privately for any purpose, but if publicly released in binary form,
+your modification of the source must be made publicly available under GPL:
+http://www.gnu.org/licenses/gpl-faq.html#GPLRequireSourcePostedPublic
+
+This project also contains Heathcliff74's WP7 Root Tools SDK, licensed under proprietary terms listed here:
+http://wp7roottools.com/
---
+++ wphEmailKey/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace wphEmailKey
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        string key = @"Software\Microsoft\FingerKB\AlternateMappings\{805d58c2-096a-4451-b2cb-40996fcb236d}";
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            if (WP7RootToolsSDK.Environment.HasRootAccess() != true)
+            {
+                MessageBox.Show("This application requires root access.\nMake sure you mark this app as Trusted in Root Tools");
+            }
+        }
+
+        private void TextBox_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+            if (tbEmail.Text == "email address")
+            {
+                tbEmail.Text = "";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            resetEmailkey();
+            WP7RootToolsSDK.Registry.SetMultiStringValue(WP7RootToolsSDK.RegistryHyve.LocalMachine, key, "@", new string[] { tbEmail.Text });
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            resetEmailkey();
+        }
+
+        void resetEmailkey()
+        {
+            try
+            {
+                WP7RootToolsSDK.Registry.DeleteValue(WP7RootToolsSDK.RegistryHyve.LocalMachine, key, "@");
+            }
+            catch
+            {
+            }
+        }
+    }
+}
